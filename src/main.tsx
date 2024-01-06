e29f64ffd508f4f8d38f6aa11a57f310e362c1b6
Ryhor Volsasu <g.volosov@email.com>
2024-01-06 15:54:27 +0400

    feat: add all files as in the project

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { LoginProvider } from './context/loginContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/samsa/">
       <ErrorBoundary>
         <LanguageProvider>
           <UserProvider>
